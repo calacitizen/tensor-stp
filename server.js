0842119146fa8490c91e41cd532efcba349fef8e
Валерий Р < >
2016-11-19 14:55:57 +0300

    music parse

--- server.js
+++ server.js
@@ -31,8 +31,55 @@ app.get('/ag', function(req, res) {
 })
 
 app.post('/push', function(req, res) {
+    
 
-    userSocket && userSocket.emit('music', req.body)
-    //res.send(req.body);
+    var result = {
+      payLoad: [],
+      noteNumbers: [],    
+      arpArray: [],        
+      fiveHundreds: false
+    }
+
+    
+
+    var count500=0;
+    for ( var i=0; i<8; ++i) {
+      result.payLoad.push(req.body[i]);
+      var firstNumberOfSize = req.body[i].size.toString()[0];
+      var firstNumberOfAmount =  req.body[i].amount.toString()[0];
+      
+      var note= Math.abs((+firstNumberOfSize) - (+firstNumberOfAmount));
+      if (note>7)
+        note=7;
+      result.noteNumbers.push(note);
+
+      if (req.body[i].code > 400)
+        count500++;
+    }
+
+    for (var i=0; i<6; ++i)
+    {
+      var firstNumberOfSize = req.body[i].size.toString()[0];
+      var firstNumberOfAmount =  req.body[i].amount.toString()[0];
+     
+      var note= Math.abs( (+firstNumberOfSize) - (+firstNumberOfAmount));
+      if (note>5)
+        note=5;
+      result.arpArray.push(note);
+    }
+      
+    if (count500>=2)
+        result.fiveHundreds= true;
+
+    console.log(result.noteNumbers);
+    console.log(result.arpArray);
+    console.log(result.fiveHundreds);
+
+    userSocket && userSocket.emit('music', result);
+
+
+
+
+
     res.sendStatus(200);
 });
