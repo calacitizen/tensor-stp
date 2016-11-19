6cd53567bb0cf67c53cbd1814916293a6eaf9ed3
DrhF <mr.fedulow@gmail.com>
2016-11-19 10:52:00 +0300

    New version of parse and process

---
+++ process.py
@@ -0,0 +1,9 @@
+import SendingMusic
+
+class Process:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def start(self):
+        sm = SendingMusic(self.filename)
+        sm.sending()
